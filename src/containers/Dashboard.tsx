735896ac749fa8622264dc6dc54e7a31957b471d
Jozef Cipa <cipa.jozef@gmail.com>
2024-09-03 08:58:03 +0200

    feat: update dashboard UI

--- src/containers/Dashboard.tsx
+++ src/containers/Dashboard.tsx
@@ -1,28 +1,46 @@
 import { ObstacleDetector } from './ObstacleDetector.tsx'
 import { Toggle } from '../components/Toggle.tsx'
 import { useState } from 'react'
+import { Compass } from '../components/Compass'
+import { BluetoothConnect } from '../components/BluetoothConnect.tsx'
 
 type Props = {
-    temperature: number
-    humidityPercentage: number
-    compassPosition: number
-    lightsOn: boolean
+  temperature: number
+  humidityPercentage: number
+  compassPosition: number
+  lightsOn: boolean
+  onBluetoothConnect: () => Promise<void>
 }
 
 export const Dashboard = (props: Props) =>{
     const [lightsOn, setLightsOn] = useState(props.lightsOn)
-
     return  (
       <div>
-          <ObstacleDetector distance={18}/>
-          <div>Temperature: {props.temperature}°C</div>
-          <div>Humidity {props.humidityPercentage}%</div>
-          <div>Compass: {props.compassPosition}°</div>
-          <div>Lights:  <Toggle checked={lightsOn} onChange={(checked) => {
-              setLightsOn(checked)
-              console.log(`lights set to "${checked}"`)}
-          }/>
+        <ObstacleDetector distance={18}/>
+        <div className="flex items-center">
+          <div className="flex-1 text-left">
+            <div className="grid grid-cols-2 gap-1">
+              <div>Temperature</div>
+              <strong className="text-blue-500">{props.temperature}°C</strong>
+              <div>Humidity</div>
+              <strong className="text-blue-500">{props.humidityPercentage}%</strong>
+              <div>Lights</div>
+              <Toggle
+                checked={lightsOn}
+                onChange={(checked) => {
+                  setLightsOn(checked)
+                  console.log(`lights set to "${checked}"`)
+                }
+                }/>
+              <div className="col-span-2">
+                <BluetoothConnect isLoading={false /* todo */} connected={false /* bluetooth.isConnected */} onClick={props.onBluetoothConnect}/>
+              </div>
+            </div>
           </div>
+          <div className="flex-1">
+            <Compass position={props.compassPosition}/>
+          </div>
+        </div>
       </div>
     )
 }
--- src/containers/MotorControlButtons.tsx
+++ src/containers/MotorControlButtons.tsx
@@ -9,7 +9,7 @@ type ButtonProps = Props & { direction: Direction }
 
 const Button = (props: ButtonProps) => (
   <button
-    className="border text-center content-center text-5xl text-slate-800 bg-slate-200 border rounded-xl"
+    className="border text-center content-center text-5xl text-slate-800 bg-[#e3e4e7] border rounded-xl hover:bg-[#dcdde1] focus:hover:bg-[#dcdde1] duration-100"
     onTouchStart={() => props.onButtonDown(props.direction)}
     onTouchEnd={props.onButtonUp}
   >
